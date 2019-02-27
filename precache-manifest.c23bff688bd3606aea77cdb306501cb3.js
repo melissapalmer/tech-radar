789ef9e19167541b7ff14f861d0469e1a9200da6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-27 19:53:18 +0000

    Deploy melissapalmer/tech-radar to github.com/melissapalmer/tech-radar.git:gh-pages

---
+++ precache-manifest.c23bff688bd3606aea77cdb306501cb3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "336854f248d5f9d13233",
+    "url": "/tech-radar/static/js/runtime~main.336854f2.js"
+  },
+  {
+    "revision": "c9e46a0391b90154f87a",
+    "url": "/tech-radar/static/js/main.c9e46a03.chunk.js"
+  },
+  {
+    "revision": "e0bbd7719fc5825f10e3",
+    "url": "/tech-radar/static/js/1.e0bbd771.chunk.js"
+  },
+  {
+    "revision": "c9e46a0391b90154f87a",
+    "url": "/tech-radar/static/css/main.208998fe.chunk.css"
+  },
+  {
+    "revision": "87e31feddcb4711d562ab0b5645bd2ac",
+    "url": "/tech-radar/index.html"
+  }
+];
